c13cfb72a8fd583e1b6886279e14c5035dd5f009
chathuranga-rupasinghe <chathuham@gmail.com>
2020-12-16 23:09:18 +0800

    create script for finding files which includes text todo and sample project

---
+++ todoFinder.js
@@ -0,0 +1,39 @@
+
+const { promisify } = require("util");
+const { resolve } = require("path");
+const fs = require("fs");
+const readdir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
+const readFile = promisify(fs.readFile);
+
+async function getFiles(directory) {
+    const subdirectories = await readdir(directory);
+
+    await Promise.all(
+        subdirectories.map(async (subdirectory) => {
+            const res = resolve(directory, subdirectory);
+
+            if ((await stat(res)).isDirectory()) {
+                return getFiles(res);
+            } else {
+                return readFile(res, "utf8", function (err, data) {
+                    if (err) throw err;
+                    if (data.toString().match(/todo/i)) {
+                        console.log(res);
+                    }
+                });
+            }
+        })
+    );
+}
+
+function runScript() {
+    if (process.argv.length > 2) {
+        getFiles(process.argv[2]);
+    } else {
+        console.error("please enter relative or absolute path of derectory as arguement");
+    }
+}
+
+runScript();
+
